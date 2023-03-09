d656518b1842dafba55d8c5ff30eeea8b74b3552
qeWait <pavl0b4kun@gmail.com>
2023-03-09 17:23:37 +0200

    init

---
+++ src/main/java/org/example/Entity/User.java
@@ -0,0 +1,26 @@
+package org.example.Entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "created_at")
+    private Date insertTime;
+}
