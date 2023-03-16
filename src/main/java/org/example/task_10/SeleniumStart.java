fbe0ec87373e6a015d301a403231cc2bcd44097b
qeWait <pavlobakun21@gmail.com>
2023-03-16 17:06:12 +0200

    upd

--- src/main/java/org/example/task_10/SeleniumStart.java
+++ src/main/java/org/example/task_10/SeleniumStart.java
@@ -1,28 +1,19 @@
 package org.example.task_10;
 
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumStart {
     public static void main(String[] args) {
-//        System.setProperty("webdriver.edge.driver","driver/msedgedriver.exe");
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://www.google.com");
-
-
         EdgeDriverManager.getInstance().setup();
         EdgeOptions options = new EdgeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver cDriver = new EdgeDriver(options);
-        cDriver.get("https//google.com/");
+        cDriver.get("https://jsonplaceholder.typicode.com");
 
-//        cDriver.close();
-//        cDriver.quit();
+        cDriver.close();
+        cDriver.quit();
     }
 }
