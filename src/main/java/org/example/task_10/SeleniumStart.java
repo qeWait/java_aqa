eefe7e9c0f541f1a54f5e097c4fd00acf38da732
qeWait <pavlobakun21@gmail.com>
2023-03-16 14:32:48 +0200

    sync

--- src/main/java/org/example/task_10/SeleniumStart.java
+++ src/main/java/org/example/task_10/SeleniumStart.java
@@ -1,5 +1,6 @@
 package org.example.task_10;
 
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,7 +8,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class SeleniumStart {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.google.com");
 
-        WebDriver chromeDriver = new ChromeDriver();
+        //2
+
+        ChromeDriverManager.getInstance().setup();
+        WebDriver cDriver = new ChromeDriver();
+        cDriver.get("https//www.google.com");
     }
 }
