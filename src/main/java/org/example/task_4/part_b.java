e83af9dcf40354970d1e01613a620e805ded67e3
qeWait <pavl0b4kun@gmail.com>
2023-03-22 23:36:48 +0200

    task 4 completed

--- src/main/java/org/example/task_4/part_b.java
+++ src/main/java/org/example/task_4/part_b.java
@@ -1,4 +1,63 @@
 package org.example.task_4;
 
+import java.lang.reflect.Constructor;
+
 public class part_b {
+//    Create your own custom class with a few constructors, and make an object for it.
+//    Use getDeclaredConstructors() to print out all the constructors of the class and their parameter types.
+//    Choose a specific constructor and create a new instance of the class using newInstance().
+//    Print out your results or processing progress
+
+    public static void main(String[] args) {
+        try {
+            Class<?> customClass = CustomClass.class;
+
+            Constructor<?>[] constructors = customClass.getDeclaredConstructors();
+
+            for (Constructor<?> constructor : constructors) {
+                System.out.println("Constructor: " + constructor);
+                System.out.print("Parameter types: ");
+                for (Class<?> paramType : constructor.getParameterTypes()) {
+                    System.out.print(paramType.getName() + ", ");
+                }
+                System.out.println();
+            }
+
+            Constructor<?> chosenConstructor = customClass.getDeclaredConstructor(int.class, String.class);
+            CustomClass newInstance = (CustomClass) chosenConstructor.newInstance(42, "example");
+
+            System.out.println("Result: " + newInstance);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class CustomClass {
+        private int number;
+        private String text;
+
+        public CustomClass() {
+            this.number = 0;
+            this.text = "";
+        }
+
+        public CustomClass(int number) {
+            this.number = number;
+            this.text = "";
+        }
+
+        public CustomClass(int number, String text) {
+            this.number = number;
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return "CustomClass{" +
+                    "number=" + number +
+                    ", text='" + text + '\'' +
+                    '}';
+        }
+    }
 }
--- src/main/java/org/example/task_4/part_c.java
+++ src/main/java/org/example/task_4/part_c.java
@@ -1,4 +1,25 @@
 package org.example.task_4;
 
 public class part_c {
+//    Create a generic interface that defines a method that takes a parameter of any type,
+//    and implement the interface with a concrete class
+//    that specifies the type using interface and implements.
+
+    public static void main(String[] args) {
+        ConcreteClass concrete = new ConcreteClass();
+        concrete.print("Hello, World!");
+    }
+
+    public interface GenericInterface<T> {
+        void print(T input);
+    }
+
+    public static class ConcreteClass implements GenericInterface<String> {
+        @Override
+        public void print(String input) {
+            System.out.println(input);
+        }
+    }
+
+
 }
